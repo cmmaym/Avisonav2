81db251dab29be68c25cd4e143360e06172c114f
unknown <jmartinezd@CIOHARHIDNAU02.dimar.mil.co>
2018-11-30 13:20:46 -0500

    Se eliminaron algunos seeder inecesarios y se agrego el seeder de symbolType

---
+++ database/seeds/SymbolTypeTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SymbolTypeTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('symbol_type')->insert([
+            [
+                'title' => 'Ayuda a la navegación',
+                'code'          => 'A1',
+                'created_at'    => new \DateTime('now'),
+                'updated_at'    => new \DateTime('now'),
+                'created_by'    => 'jmartinezd',
+                'updated_by'    => 'jmartinezd'
+            ],
+            [
+                'title' => 'Peligro a la navegación',
+                'code'          => 'D1',
+                'created_at'    => new \DateTime('now'),
+                'updated_at'    => new \DateTime('now'),
+                'created_by'    => 'jmartinezd',
+                'updated_by'    => 'jmartinezd'
+            ]
+        ]);
+    }
+}
